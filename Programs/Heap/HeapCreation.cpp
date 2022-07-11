651f9b7639ae620792ba64256b1537b4302f89dc
Vedprakash Sharma <v.sharma@tricentis.com>
2022-07-11 10:11:05 +0530

    completed the inert method in heap

--- Programs/Heap/HeapCreation.cpp
+++ Programs/Heap/HeapCreation.cpp
@@ -8,12 +8,21 @@ class Heap
         int size;
         Heap()
         {
+            arr[0]=-1;
             size=0;
+        }
+
+        void swap(int a,int b)
+        {
+            int temp= a;
+            a=b;
+            b=temp;
         }
 
         void insert(int val)
         {
-            int index=size+1;
+            size=size+1;
+            int index=size;
             arr[index]=val;
             while(index>1)
             {
@@ -21,7 +30,10 @@ class Heap
                 if(arr[parent]<arr[index])
                 {
                     swap(arr[parent],arr[index]);
-                    index=parent;
+                    // int temp=arr[parent];
+                    // arr[parent]=arr[index];
+                    // arr[index]=temp;
+                     index=parent;
                 }
                 else
                 {
@@ -29,4 +41,26 @@ class Heap
                 }
             }
         }
+
+        void print()
+        {
+            for(int i=1;i<=size;i++)
+            {
+                cout<<arr[i]<<" ";
+            }
+            cout<<endl;
+        }
 };
+
+int main()
+{
+    Heap h;
+    h.insert(10);
+    h.insert(30);
+    h.insert(5);
+    h.insert(55);
+    h.insert(67);
+    h.insert(34);
+    h.print();
+
+}
